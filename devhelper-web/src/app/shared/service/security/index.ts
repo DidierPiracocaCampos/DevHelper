0efad8bf6a6b9331e9ed1a320a4b6905c6732db5
Didier Estiben Piracoca Campos <98455168+diperpiracoca@users.noreply.github.com>
2026-05-03 13:59:01 +0200

    delete security folder

--- devhelper-web/src/app/shared/service/security/index.ts
+++
@@ -1,1 +0,0 @@
-export { VaultSecurity } from "./services/vault-security";
--- devhelper-web/src/app/shared/service/security/models/unlock-key.model.ts
+++
@@ -1,7 +0,0 @@
-export interface UnlockKeyI {
-    id?:string;
-    encryptedMasterKey: ArrayBuffer;
-    salt?: Uint8Array;
-    iv: Uint8Array;
-    params: any;
-}
