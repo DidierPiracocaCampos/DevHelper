f06633c2acae1908ed9fd5f0c551d349d720db3c
Didier Estiben Piracoca Campos <98455168+diperpiracoca@users.noreply.github.com>
2026-05-08 18:22:12 +0200

    mejoras esteticas para los botones

--- devhelper-web/src/app/shared/components/ui-button/button.ts
+++ devhelper-web/src/app/shared/components/ui-button/button.ts
@@ -2,19 +2,19 @@ import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 
 @Component({
-  selector: 'sh-button',
+  selector: 'ui-button',
   imports: [NgClass],
   templateUrl: './button.html',
   styleUrl: './button.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Button {
-  severity = input<'neutral' | 'primary' | 'secondary' | 'accent' | 'info' | 'success' | 'warning' | 'error'>();
+  severity = input<'neutral' | 'primary' | 'secondary' | 'accent' | 'info' | 'success' | 'warning' | 'error'>('primary');
   type = input<'submit' | 'button' | 'reset'>();
   btnClass = input<string>('');
   icon = input<string>();
   label = input<string>();
-  isLoandig = input<boolean>(false);
+  isLoading = input<boolean>(false);
   disabled = input<boolean>(false);
   onClick = output();
 
