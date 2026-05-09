87e0141a13594a0608834fa59aedad1c36a1cc1c
Didier Estiben Piracoca Campos <98455168+diperpiracoca@users.noreply.github.com>
2026-05-09 21:00:51 +0200

    refactor: move project files from devhelper-web/ to root

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    firebaseConfig: {
+        projectId: "devhelper-a61ef",
+        appId: "1:35426644698:web:aad2c5afac9d3aa2276be3",
+        storageBucket: "devhelper-a61ef.firebasestorage.app",
+        apiKey: "AIzaSyAXJdhVQv-xk6XIAvUpAVYzm14xXqqffbw",
+        authDomain: "devhelper-a61ef.firebaseapp.com",
+        messagingSenderId: "35426644698",
+        measurementId: "G-19T6CPNND6",
+        projectNumber: "35426644698",
+        version: "2"
+    },
+    nasaApiKey: 'r0yqrzeYbPpGwIUKPoK77Ujxd5Vg8RDO2xjObvWu'
+};
