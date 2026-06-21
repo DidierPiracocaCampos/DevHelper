5822a174aca72b283db2b6104d54acad02f914df
Didier Piracoca <piracocadidier@gmail.com>
2026-06-21 18:40:02 +0200

    refactor: renombrar carpeta environments a environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
+import { environment } from '../environment/environment';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ErrorHandler } from '@angular/core';
 import { GlobalErrorHandler } from './shared/handler/global-error-handler';
--- src/app/home/service/nasa-picture.ts
+++ src/app/home/service/nasa-picture.ts
@@ -1,6 +1,6 @@
 import { httpResource } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environment/environment';
 import { NasaPictureI } from '../domain/nasa-picture.interface';
 
 @Injectable({
